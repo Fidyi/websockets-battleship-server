a710816db3766d84f25f2050dd01bf7825f19888
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-10-27 22:12:30 +0300

    init: start task

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { httpServer } from "./src/http_server/index.js";
 
 const HTTP_PORT = 8181;
+const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+console.log(`Starting WebSocket server on port ${WS_PORT}`);
+startWebSocketServer(WS_PORT);
